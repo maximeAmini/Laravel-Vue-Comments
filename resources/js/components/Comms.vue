292e567c69894f1cc8e08899e8152ef06f4f46b2
Maxime Amini <maximeamini1@gmail.com>
2020-08-18 04:32:50 +0100

    init

---
+++ resources/js/components/Comms.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="comms">
+
+    <div class="comms-list">
+        <div class="comm" v-for="comm in comms" :key ="comm.id">{{comm}}</div>
+    </div>
+
+    <form action="" class="comms-form" @submit.prevent="addComm">
+        <input type="text" placeholder="Laisser votre commentaire" v-model="comm" >
+    </form>
+
+</div>
+
+</template>
+
+<script>
+    export default {
+
+        data (){
+            return {
+                comm:"",
+                comms : ['1er comm','2eme comm','3eme comm']
+            }
+        }
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        {{nom}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                'nom': 'maxime amini'
+
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
